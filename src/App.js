4ba0c4ba5d7dcb4022b238195282066c52eb764f
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-11-07 02:15:49 +0000

    lasted update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./styles/style.css";
+import sun from "./images/icon-sun.svg";
+import moon from "./images/icon-moon.svg";
 function App() {
+  const [theme, setTheme] = useState(true);
+  const [completed, setCompleted] = useState(true);
+  const themeIcon = theme ? sun : moon;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      class={`container ${theme ? "dark" : "light"}`}
+      data-theme={theme ? "dark" : "light"}
+    >
+      <div class="wrapper">
+        <div class="header">
+          <h1 class="logo">Todo</h1>
+          <img
+            onClick={() => setTheme(!theme)}
+            src={themeIcon}
+            alt="dark-light theme"
+            />
+        </div>
+        <div class="input">
+          <div class="circle"></div>
+          <input type="text" placeholder="Currently typing" />
+        </div>
+        <div class="list">
+          <div class="todo"
+            onClick={() => setCompleted(!completed)}
+          >
+            <div
+              class={`circle ${completed ? "completed" : "uncompleted"}`}
+              ></div>
+            <div class={`text ${completed ? "completed" : "uncompleted"}`}>
+              Complete Todo App on Frontend Mentor
+            </div>
+          </div>
+          <div class="todo"
+            onClick={() => setCompleted(!completed)}
+          >
+            <div
+              class={`circle ${completed ? "completed" : "uncompleted"}`}
+              ></div>
+            <div class={`text ${completed ? "completed" : "uncompleted"}`}>
+              Complete Todo App on Frontend Mentor
+            </div>
+          </div>
+          <div class="tool">
+            <div class="items-left top">
+              <span class="num">0</span>
+              <p>items left</p>
+            </div>
+            <div class="filter">
+              <p>All</p>
+              <p>Active</p>
+              <p>completed</p>
+            </div>
+            <p class="clear top">Clear Completed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

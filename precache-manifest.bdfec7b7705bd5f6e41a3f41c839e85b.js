ac1b198e7b2ed8f99e97934ab397c8c9cab0594e
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-11-11 16:13:35 +0000

    Updates

---
+++ precache-manifest.bdfec7b7705bd5f6e41a3f41c839e85b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe70279b5be1b53464cf7ffaa06a9922",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "135d2c21259b7d8ff742",
+    "url": "/todo-app/static/css/main.257882e5.chunk.css"
+  },
+  {
+    "revision": "7ae33fc85422bf0f29c2",
+    "url": "/todo-app/static/js/2.42fdb0f3.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/todo-app/static/js/2.42fdb0f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135d2c21259b7d8ff742",
+    "url": "/todo-app/static/js/main.bc8c1cf7.chunk.js"
+  },
+  {
+    "revision": "f0a118bf01c0c550f709",
+    "url": "/todo-app/static/js/runtime-main.73fa848f.js"
+  },
+  {
+    "revision": "02105f02a8dba33f6c8bafac6a9b0979",
+    "url": "/todo-app/static/media/bg-desktop-dark.02105f02.jpg"
+  },
+  {
+    "revision": "988cb74c73a65433889e51b150c1a8f9",
+    "url": "/todo-app/static/media/bg-desktop-light.988cb74c.jpg"
+  },
+  {
+    "revision": "5742160ca6ea943a508cc5763e8cd312",
+    "url": "/todo-app/static/media/icon-check.5742160c.svg"
+  },
+  {
+    "revision": "fe68d57f09f867dbbd1ff592c3b9a017",
+    "url": "/todo-app/static/media/icon-cross.fe68d57f.svg"
+  },
+  {
+    "revision": "e66b8c621b1f209e44a7c955c0e5e809",
+    "url": "/todo-app/static/media/icon-moon.e66b8c62.svg"
+  },
+  {
+    "revision": "b3182f3b0ccf1d65cae48b64861b83a9",
+    "url": "/todo-app/static/media/icon-sun.b3182f3b.svg"
+  }
+]);
